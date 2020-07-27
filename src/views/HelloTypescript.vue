a9f24667494cf4b952af49e410e964bf8266d957
webrsb <webrsb@gmail.com>
2020-07-27 21:41:19 +0800

    2020/07/24 題目

--- src/views/HelloTypescript.vue
+++ src/views/HelloTypescript.vue
@@ -5,6 +5,22 @@
 </template>
 
 <script lang="ts">
+/**
+  * 2020/07/24 解答
+  */
+let a:number = 10
+let b:string = '10'
+let c:boolean = true
+let d:undefined = undefined
+let e:null = null
+let f:any
+let g:number[] = [1, 2]
+let h:Array<number|string> = [1, '2']
+let i:{ x: number, y: string } = { x: 5, y: '6'}
+
+/**
+  * 2020/07/17 解答
+  */
 function helloWorld(params:string):void {
   console.log(params)
 }
